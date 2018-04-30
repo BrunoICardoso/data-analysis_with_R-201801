6ac14a12f63ddaeb0ad26c5e7f2e4222e54c5879
BrunoICardoso <bruno.inacio88@gmail.com>
2018-04-30 01:55:04 +0000

    Termino dos exercicios

--- aula-02/05_atividade.R
+++ aula-02/05_atividade.R
@@ -6,6 +6,7 @@ load("aula-02/data/dados_exercicio.RData")
 
 ### 1 ####
 ## Inicie mostrando uma prévia do conteúdo da variável acessos_alunos
+## 
 ## Dica 1: No material sobre estruturas de dados vimos como exibir uma prévia do conteúdo de uma variável com 2 funções diferentes
 ## Dica 2: Na primeira aula vimos uma função do RStudio que permite visualizar o conteúdo de uma variável, mas neste caso 
 ##         quero ver uma saída na Console.
@@ -14,53 +15,42 @@ str(acessos_alunos)
 
 ### 2 ###
 ## Quantos elementos a variável acessos_alunos possui? Utilize uma função do R que retorna o tamanho da variável.
-## Dica: Vimos um exemplo no mesmo material sobre estruturas de dados
-print(length(acessos_alunos))
-### # ###
 
+## Dica: Vimos um exemplo no mesmo material sobre estruturas de dados
+### # ###
+paste("A variável 'acessos_alunos' possui ", length(acessos_alunos), " elementos.")
 
 
 ### 3 ###
 ## Utilizando o seu código de aluno da Uniritter como nome de um valor da lista, imprima uma linha informando quantos acessos
 ## você fez. A linha deve ser impressa na Console, com um texto que diga o seu código de aluno e o valor conforme o seguinte exemplo:
 ## "O aluno <alu...> realizou N acessos."
+
 ## Dica 1: Utilize a função paste() para composição do texto que será impresso. 
 ## Dica 2: Vimos exemplos disto nos materiais dos tipos numéricos e das estruturas de dados.
-print(paste("O aluno alu201830194 realizou  ",acessos_alunos["alu201830194"]," Acessos"))
 ### # ###
+paste("O aluno alu201830194 realizou ", acessos_alunos$alu201830194, " acessos.")
+
 
 ### 4 ###
 ## A operação abaixo cria um vetor com todas as quantidades de acessos por aluno.
 acessos <- unlist(acessos_alunos)
+
 ## Após a criação deste vetor, determine quantos colegas fizeram mais acessos que você.
 ## Faça isso em 3 etapas: 
 ## 1. Crie uma variável com o resultado de um teste de comparação (relacional) entre o seu número de acessos e os demais.
-
-for (c in acessos){
-    print(paste("Numero de acessos",acessos[c], "Comparado do aluno alu201830194"),acessos["alu201830194"])
-
-}
-
 ## 2. Com uma operação de indexação, crie um outro vetor contendo somente os valores maiores
-for (c in acessos){
-
-  if(c < acessos["alu201830194"])
-    print(acessos[c])
-
-
-
-}
-
-
 ## 3. Determine o tamanho do vetor da operação 2, imprimindo o resultado na Console
 ### # ###
-
+varCompar <- acessos > acessos_alunos$alu201830194
+alunos_mais_acessos <- acessos[varCompar]
+paste("Quantidade de alunos com mais acesso: ", length(alunos_mais_acessos))
 
 
 ### 5 ###
 ## Combine todas as etapas acima em uma única chamada, sem a criação dos vetores auxiliares
 ### # ###
-
+paste("Quantidade de alunos com mais acesso: ",length(acessos[acessos > acessos_alunos$alu201830194]))
 
 
 ### 6 ###
@@ -69,7 +59,7 @@ for (c in acessos){
 
 ## Dica: Lembre que falamos sobre como o R faz conversões implícitas entre o tipo lógico e tipos numéricos
 ### # ###
-
+paste("Quantidade de alunos com menos acesso: ", sum(acessos < acessos_alunos$alu201830194))
 
 
 ### 7 ###
@@ -82,13 +72,16 @@ for (c in acessos){
 ## Dica: Pode ser mais fácil se iniciar o vetor notas como uma cópia do vetor acessos, modificando os valores conforme as regras
 ## OBSERVAÇÃO :: Não avaliarei participação na forma do enunciado deste exercício. 
 ### # ###
+notas_aluno <- unlist(acessos_alunos, use.names = FALSE)
+notas_aluno[which(notas_aluno > 0 & notas_aluno < 10)] <- 1
+notas_aluno[which(notas_aluno >= 10)] <- 2
 
 
 
 ### 8 ###
 ## Visualização da quantidade de alunos com cada nota de participação. Esta não é uma atividade, apenas uma ilustração de como
 ## criar uma tabela com esta contagem
-# table(notas)
+table(notas)
 
 
 
@@ -101,6 +94,22 @@ acessos_alunos_e_guest$guest <- NA
 ## Repita as atividades 4, 5, 6, e 7 utilizando o acessos_com_guest no lugar da lista acessos_alunos.
 ## Tome o devido cuidado de sempre criar variáveis com nomes diferentes das já utilizadas! 
 
+# 4
+acessosEx9 <- unlist(acessos_alunos_e_guest)
+varComparEx9 <- acessosEx9 > acessos_alunos_e_guest$alu201830194
+alunos_com_mais_acessosEx9 <- acessosEx9[varComparEx9]
+paste("Quantidade de alunos com mais acesso: ", length(alunos_com_mais_acessosEx9))
+
+# 5
+paste("Quantidade de alunos com mais acesso: ",length(acessosEx9[acessosEx9 > acessos_alunos_e_guest$alu201830194]))
+
+# 6
+paste("Quantidade de alunos com menos acessos: ", sum(acessosEx9 < acessos_alunos_e_guest$alu201830194))
+
+# 7
+notasEx9 <- unlist(acessos_alunos_e_guest, use.names = FALSE)
+notasEx9[which(notasEx9 > 0 & notasEx9 < 10)] <- 1
+notasEx9[which(notasEx9 >= 10)] <- 2
 
 
 ### 10 ###
@@ -109,11 +118,13 @@ acessos_alunos_e_guest$guest <- NA
 
 # 1. Houve modificação no número de alunos com mais e com menos acessos que você?
 
+
 # 2. Como você conclui que o R trata comparações (operações relacionais) entre valores numéricos e NA?
+
 
 # 3. Qual o resultado do uso da função sum na presença de NA? O que você conclui sobre a operação de soma de todos os valores de
 #    um vetor na presença de NA?
 
+
 # 4. Execute o comando abaixo para ler a documentação da função sum e veja se há como modificar a chamada da função sum na presença
 #    de NAs. Teste os exemplos da página de help da função sum.
-help(sum)
